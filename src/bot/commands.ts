2f17610979d230c6fd1deedc03664bd83cdfe544
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-08-18 18:37:45 +0300

    chore: remove unused code

--- src/bot/commands.ts
+++ src/bot/commands.ts
@@ -2,5 +2,4 @@ export const commands = [
   { command: 'healthcheck', description: 'Проверка состояния' },
   { command: 'add', description: 'Добавить библиотеку' },
   { command: 'search', description: 'Найти библиотеку' },
-  // { command: 'filter', description: 'Найти библиотеки по заданному фильтру' },
 ];
