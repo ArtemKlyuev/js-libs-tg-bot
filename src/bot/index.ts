5b8864685d20c5399dc7cae1a57fb0a3e0cba590
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-08-23 19:29:02 +0300

    fix: typo

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -2,4 +2,4 @@ import { startBot } from './bot';
 
 startBot()
   .then(() => console.log('Бот успешно запущен!'))
-  .catch((error) => console.error(`Ошибка пи запуске бота!\nОшибка:${error}`));
+  .catch((error) => console.error(`Ошибка при запуске бота!\nОшибка:${error}`));
