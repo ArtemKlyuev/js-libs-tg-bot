9cf43773012a32b05b6ec5f6a103ef79e4699edc
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-10-10 22:16:52 +0500

    feat: make `Input` controlled

--- packages/client/src/components/Input/Input.tsx
+++ packages/client/src/components/Input/Input.tsx
@@ -1,16 +1,13 @@
-import { forwardRef } from 'react';
+import { ChangeEvent, forwardRef } from 'react';
 
 interface Props {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
 }
 
-export const Input = forwardRef<HTMLInputElement, Props>(({ placeholder }, ref) => {
+export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
   return (
-    <input
-      ref={ref}
-      type="text"
-      placeholder={placeholder}
-      className="input input-bordered w-full max-w-xs"
-    />
+    <input ref={ref} type="text" className="input input-bordered w-full max-w-xs" {...props} />
   );
 });
