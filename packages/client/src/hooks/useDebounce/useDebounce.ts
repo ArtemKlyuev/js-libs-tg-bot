fb05b1852c9e8925374cef6db8252f0db11a6bd9
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-10-10 20:27:02 +0500

    feat: add `useDebounce` hook

---
+++ packages/client/src/hooks/useDebounce/useDebounce.ts
@@ -0,0 +1,24 @@
+import { useRef, useEffect } from 'react';
+import { Settings, DebouncedFunction, debounce } from 'common/utils';
+
+interface Options<Callback extends (...args: any) => any> {
+  callback: Callback;
+  wait?: number;
+  settings?: Settings;
+}
+
+export const useDebounce = <Callback extends (...args: any) => any>({
+  callback,
+  wait,
+  settings,
+}: Options<Callback>): DebouncedFunction<Callback> => {
+  const debouncedCallback = useRef(debounce(callback, wait, settings)).current;
+
+  useEffect(() => {
+    return () => {
+      debouncedCallback.cancel();
+    };
+  }, [debouncedCallback]);
+
+  return debouncedCallback;
+};
