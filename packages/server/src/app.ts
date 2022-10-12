a49cd4f86689b642dcf33991d04e550a9ac590d7
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-10-12 20:12:25 +0400

    feat: start `check-library` route

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -6,9 +6,17 @@ app.get('/', async (request, reply) => {
   return { hello: 'world' };
 });
 
-app.get('/check-library', async (request, reply) => {
-  return { hello: 'world' };
-});
+app.get(
+  '/check-library',
+  { schema: { querystring: { name: { type: 'string' } } } },
+  async (request, reply) => {
+    if (!request.query.name) {
+      return reply.code(400).send({ error: `you should specify "name" param` });
+    }
+    // return `requested library: ${String(request.query)}`;
+    return `requested library: kek`;
+  },
+);
 
 const start = async () => {
   try {
