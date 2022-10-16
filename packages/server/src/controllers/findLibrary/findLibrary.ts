817f9ccd7fcc56ec94292b32de2ac69f67b7c1f7
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-10-16 16:06:13 +0400

    feat: add `findLibrary` controller

--- packages/server/src/controllers/findLibrary/findLibrary.ts
+++ packages/server/src/controllers/findLibrary/findLibrary.ts
@@ -1,1 +1,10 @@
-export const findLibrary = async (name: string): Promise<any> => {};
+import { DatabaseRepository, SearchLibraryResult } from '../../repository';
+
+export const findLibrary = async (
+  name: string,
+  dbRepository: DatabaseRepository,
+): Promise<SearchLibraryResult> => {
+  const result = await dbRepository.searchLibraryByName(name);
+
+  return result;
+};
