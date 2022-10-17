1ea31cc93250e07b9be4acbba647e93c5d39623e
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-10-17 17:30:40 +0400

    refactor: types for `Repository`

--- packages/server/src/repository/Database/Repository.ts
+++ packages/server/src/repository/Database/Repository.ts
@@ -2,16 +2,18 @@ import { Either } from '@sweet-monads/either';
 
 import { FindError } from '@services';
 
-export interface LibraryData {
-  Name: string;
-  Platform: string | null;
-  'Repo link': string | null;
-  'NPM weekly downloads': number | null;
-  Tags: string[];
-  Summary: string | null;
-  Status: string | null;
-  'Score /5': string | null;
-  Review: string | null;
+// TODO: избавиться от `null` у `summary`, `repoURL`, `npmDownloads`, `githubStars`, `status`
+export interface LegacyLibraryData {
+  name: string;
+  platform: string;
+  tags: string[];
+  summary: string | null;
+  repoURL: string | null;
+  npmDownloads: number | null;
+  githubStars: number | null;
+  status: string | null;
+  score: string | null;
+  review: string | null;
 }
 
 export interface Properties {
@@ -34,8 +36,8 @@ export interface Library {
   review?: string;
 }
 
-export type SearchResult = Either<FindError, LibraryData[]>;
-export type SearchLibraryResult = Either<FindError, LibraryData>;
+export type SearchResult = Either<FindError, LegacyLibraryData[]>;
+export type SearchLibraryResult = Either<FindError, LegacyLibraryData>;
 export type PropertiesResult = Either<Error, Properties>;
 export type AddResult = Either<Error, void>;
 
