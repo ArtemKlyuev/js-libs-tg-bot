b8ba6093a5052c1f5b023b798f4922035ae15073
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-10-20 11:00:00 +0400

    feat: add interfaces

--- packages/server/src/services/Database/Database.ts
+++ packages/server/src/services/Database/Database.ts
@@ -12,11 +12,25 @@ export interface IdentifiedData<Data> {
 }
 
 export type InsertResult = Either<InsertError, void>;
+export type FiltersResult<Error, Data> = Either<Error, IdentifiedData<Data>[]>;
 export type QueryResult<Error, Data> = Either<Error, IdentifiedData<Data>[]>;
 export type DatabaseInfo<Error, Data> = Either<Error, Data>;
 
+interface Filters {}
+
+interface Sort {
+  property: string;
+  direction: 'ascending' | 'descending';
+}
+
+interface Config {
+  filters: Filters;
+  sort?: Sort[];
+}
+
 export interface Database<InsertData, QueryData, DatabaseSignature> {
   insert: (data: InsertData) => Promise<InsertResult>;
+  findByFilters: (config: Config) => Promise<FiltersResult<FindError, QueryData>>;
   findByQuery: (query: string) => Promise<QueryResult<FindError, QueryData>>;
   getDatabaseInfo: () => Promise<DatabaseInfo<Error, DatabaseSignature>>;
 }
