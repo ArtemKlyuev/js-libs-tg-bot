66c258bf30101004f95b787e7360b853cbff6f01
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-10-21 17:16:31 +0400

    feat(utils): add `ToTS` type

--- packages/server/src/utils/JSONSchema/JSONSchema.ts
+++ packages/server/src/utils/JSONSchema/JSONSchema.ts
@@ -1,7 +1,9 @@
-import { z } from 'zod';
-import zodToJsonSchema from 'zod-to-json-schema';
+import { z, ZodType } from 'zod';
+import { zodToJsonSchema } from 'zod-to-json-schema';
 
 export class JSONSchema {
   static defineSchema = z;
   static toJSONSchema = zodToJsonSchema;
 }
+
+export type ToTS<T extends ZodType> = z.infer<T>;
