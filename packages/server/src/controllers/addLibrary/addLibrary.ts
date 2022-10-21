b2f4adbe8fcf5610bb829f540dfd1dab6ea2b14f
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-10-21 15:45:53 +0400

    refactor(controllers): add early return

--- packages/server/src/controllers/addLibrary/addLibrary.ts
+++ packages/server/src/controllers/addLibrary/addLibrary.ts
@@ -4,13 +4,21 @@ import { FetchError, PackageNameError } from 'common/services';
 import { Dependencies, LibraryData } from './types';
 import { getLibraryWithGithubInfo } from './helpers';
 
-type AddLibraryResult = Either<Error | (Error | PackageNameError | FetchError)[], void>;
+type Errors = Error | PackageNameError | FetchError;
+
+type AddLibraryResult = Either<Errors | Errors[], void>;
 
 export const addLibrary = async (
   libraryData: LibraryData,
   { dbRepository, github, npmRegistry }: Dependencies,
 ): Promise<AddLibraryResult> => {
   const npm = await npmRegistry.getPackageInfo(libraryData.name);
+
+  if (npm.isLeft()) {
+    // @ts-expect-error Ругается, что `PackageInfo` не соответствует типу `void`, но
+    // состояние `Either` - `left`, поэтому в `mapRight` ничего не попадёт
+    return npm;
+  }
 
   const eitherDownloads = (
     await npmRegistry.getPackageDownloads(libraryData.name, { point: 'last-week' })
