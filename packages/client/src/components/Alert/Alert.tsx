6e884f9351caac76155bfb18b527300d2f2be779
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-03 00:19:35 +0400

    feat(components): add `Alert`

---
+++ packages/client/src/components/Alert/Alert.tsx
@@ -0,0 +1,32 @@
+import cn from 'classnames';
+
+import { CircleButton } from '../CircleButton';
+
+interface Props {
+  type: 'success' | 'error';
+  message: string;
+}
+
+export const Alert = ({ type, message }: Props) => {
+  return (
+    <div className={cn('alert', `alert-${type}`, 'shadow-lg')}>
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="stroke-current flex-shrink-0 h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>{message}</span>
+        <CircleButton />
+      </div>
+    </div>
+  );
+};
