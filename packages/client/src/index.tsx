cd756aaa376f6d385671d4ea70e6f4d4bd789426
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-03 14:14:37 +0400

    feat(app): add routes

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,17 +1,39 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+import { Error, Root, AddLibrary } from '@pages';
+import { Layout } from '@components';
 
 import { App } from './App';
 
 import './index.scss';
 
+const root = document.getElementById('root')!;
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <Error />,
+  },
+  {
+    path: '/library/add',
+    element: (
+      <Layout title="Добавление библиотеки">
+        <AddLibrary />
+      </Layout>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(root).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      {/* <App /> */}
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
