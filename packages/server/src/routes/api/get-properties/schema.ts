25c5ae0d69c41b97d393ec1ca51cd7e426d1cb60
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-03 22:44:38 +0400

    fix(server/routes): schema

--- packages/server/src/routes/api/get-properties/schema.ts
+++ packages/server/src/routes/api/get-properties/schema.ts
@@ -2,18 +2,30 @@ import { FastifySchema } from 'fastify';
 
 import { ToTS, JSONSchema } from '@utils';
 
+const nonEmptyString = JSONSchema.defineSchema.string().min(1);
+
 const definedResponseSchema = JSONSchema.defineSchema.object({
   error: JSONSchema.defineSchema.string().or(JSONSchema.defineSchema.null()),
   properties: JSONSchema.defineSchema
     .object({
-      id: JSONSchema.defineSchema.string().min(1),
-      name: JSONSchema.defineSchema.string().min(1),
-      type: JSONSchema.defineSchema.string().min(1),
+      id: nonEmptyString,
+      name: nonEmptyString,
+      type: nonEmptyString,
       value: JSONSchema.defineSchema
         .string()
         .min(1)
-        .or(JSONSchema.defineSchema.string().min(1).array().nonempty())
-        .or(JSONSchema.defineSchema.number()),
+        .or(nonEmptyString.array().nonempty())
+        .or(JSONSchema.defineSchema.number())
+        .or(
+          JSONSchema.defineSchema
+            .object({
+              id: nonEmptyString,
+              name: nonEmptyString,
+            })
+            .array()
+            .nonempty(),
+        )
+        .or(JSONSchema.defineSchema.null()),
     })
     .array()
     .or(JSONSchema.defineSchema.void().array().length(0)),
