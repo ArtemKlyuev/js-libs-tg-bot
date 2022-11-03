f8f1f06b239843827276c46b08858d926ac99498
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-03 17:25:15 +0400

    feat(hooks): add `HttpRequest` interface

--- packages/client/src/hooks/useServices/useServices.ts
+++ packages/client/src/hooks/useServices/useServices.ts
@@ -1,8 +1,11 @@
 import { createContext, useContext } from 'react';
+import { HttpRequest } from 'common/services';
 
-interface Services {}
+interface Services {
+  httpRequest: HttpRequest;
+}
 
-const ServicesContext = createContext<Services>({});
+const ServicesContext = createContext<Services>({} as Services);
 export const ServicesProvider = ServicesContext.Provider;
 
 export const useServices = (): Services => useContext(ServicesContext);
