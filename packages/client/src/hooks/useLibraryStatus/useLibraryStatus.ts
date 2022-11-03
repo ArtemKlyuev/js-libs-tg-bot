31dbcf70a40becdfdfce4146e2c558424f41b23a
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-03 19:02:34 +0400

    feat(client/hooks): finish `useLibraryStatus` hook

--- packages/client/src/hooks/useLibraryStatus/useLibraryStatus.ts
+++ packages/client/src/hooks/useLibraryStatus/useLibraryStatus.ts
@@ -2,34 +2,42 @@ import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
 
 import { PackageInfo, PackageNameError, FetchError, useNPMRegistry } from '../useNPMRegistry';
+import { useServices } from '../useServices';
+
+interface ExistPackage {
+  error: string | null;
+  exist: boolean;
+}
 
 export const useLibraryStatus = () => {
+  const { httpRequest } = useServices();
   const [library, setLibrary] = useState('');
   const NPMRegistry = useNPMRegistry();
 
-  const databaseLibraryInfo = useQuery<PackageInfo, PackageNameError | FetchError>(
+  const checkExistingLibraryInDb = useQuery<ExistPackage, PackageNameError | FetchError>(
     ['database-library-info', library],
     async () => {
-      const result = await NPMRegistry.getPackageInfo(library);
-      return result.unwrap();
+      const { response } = httpRequest.get<ExistPackage>('/library/check-existing', {
+        params: { name: library },
+      });
+      const result = await response;
+      return result.data;
     },
     { enabled: Boolean(library) },
   );
 
-  console.log({ databaseLibraryInfo });
-
-  const packageID = databaseLibraryInfo.data?.id;
-
   const npmLibraryInfo = useQuery<PackageInfo, PackageNameError | FetchError>(
-    ['npm-library-info', packageID],
+    ['npm-library-info'],
     async () => {
       const result = await NPMRegistry.getPackageInfo(library);
       return result.unwrap();
     },
-    { enabled: databaseLibraryInfo.isError },
+    { enabled: checkExistingLibraryInDb.isError },
   );
 
-  console.log({ npmLibraryInfo });
-
-  return { setLibrary };
+  return {
+    setLibrary,
+    existOnDb: checkExistingLibraryInDb.isFetched ? !checkExistingLibraryInDb.isError : null,
+    existOnNPM: npmLibraryInfo.isFetched ? !npmLibraryInfo.isError : null,
+  };
 };
