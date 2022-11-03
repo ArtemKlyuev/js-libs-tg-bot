0f4c82f0ead8f06b3ed6132956b471a9895a0bc9
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-03 22:45:06 +0400

    feat(server/routes): add `get-properties` route

--- packages/server/src/routes/api/api.ts
+++ packages/server/src/routes/api/api.ts
@@ -1,7 +1,9 @@
 import { FastifyPluginCallback } from 'fastify';
 
+import { getPropertiesRoute } from './get-properties';
 import { libraryRoutes } from './library';
 
 export const apiRoutes: FastifyPluginCallback = async (app) => {
+  app.register(getPropertiesRoute);
   app.register(libraryRoutes, { prefix: '/library' });
 };
