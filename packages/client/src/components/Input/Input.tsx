8a24e6407e02344a271ceb14f0ff4746852351bb
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-03 15:54:59 +0400

    feat(components): add `type` prop

--- packages/client/src/components/Input/Input.tsx
+++ packages/client/src/components/Input/Input.tsx
@@ -1,13 +1,14 @@
 import { ChangeEvent, forwardRef } from 'react';
 
 interface Props {
+  type?: 'text' | 'search';
   value: string;
   onChange: (event: ChangeEvent<HTMLInputElement>) => void;
   placeholder?: string;
 }
 
-export const Input = forwardRef<HTMLInputElement, Props>((props, ref) => {
+export const Input = forwardRef<HTMLInputElement, Props>(({ type = 'text', ...props }, ref) => {
   return (
-    <input ref={ref} type="text" className="input input-bordered w-full max-w-xs" {...props} />
+    <input ref={ref} type={type} className="input input-bordered w-full max-w-xs" {...props} />
   );
 });
