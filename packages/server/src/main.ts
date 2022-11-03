463195df991d5f402aac37b553786b29a7850f70
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-03 19:01:32 +0400

    feat(server): add `cors`

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,10 +1,13 @@
 import { fastifyAwilixPlugin } from '@fastify/awilix';
+import cors from '@fastify/cors';
 
 import { createApp } from './app';
 import { apiRoutes } from './routes';
 import { createDIContainer } from './diContainer';
 
 const app = createApp();
+
+await app.register(cors, { origin: true, credentials: true });
 
 app.addHook('onRequest', (request, reply, done) => {
   // TODO: auth check goes here
