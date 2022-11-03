139f3edfefd6a2e0ccfe26294ea5d233fd7ab52c
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-03 19:20:37 +0400

    refactor(client/layout): change layout

--- packages/client/src/components/Layout/Layout.tsx
+++ packages/client/src/components/Layout/Layout.tsx
@@ -9,7 +9,7 @@ export const Layout = ({ title, children }: Props) => {
   useDocumentTitle(title);
 
   return (
-    <main className="w-[375px] m-auto">
+    <main className="max-w-[414px] m-auto p-[5px]">
       <h1 className="text-3xl font-bold underline">{title}</h1>
       {children}
     </main>
