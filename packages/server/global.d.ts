310d73c2226e43aae77a3646f70b2abb0f459e4a
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-06 16:16:09 +0400

    feat(server): add types for new env variables

--- packages/server/global.d.ts
+++ packages/server/global.d.ts
@@ -2,6 +2,9 @@ interface Env {
   readonly NODE_ENV: 'development' | 'production' | 'test';
   readonly NOTION_TOKEN: string;
   readonly NOTION_DATABASE_ID: string;
+  readonly HOST: string;
+  readonly PORT: number;
+  readonly ALLOWED_ORIGINS: string[];
 }
 
 type EnvVaribales = keyof Env;
