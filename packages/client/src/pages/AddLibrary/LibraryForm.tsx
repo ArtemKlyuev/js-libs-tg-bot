e9bc1a8a29a5260748a20716ddb052664d875119
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-07 00:38:14 +0400

    refactor(client/pages): use page hooks

--- packages/client/src/pages/AddLibrary/LibraryForm.tsx
+++ packages/client/src/pages/AddLibrary/LibraryForm.tsx
@@ -1,159 +1,45 @@
-import { Fragment, useMemo } from 'react';
-import { useMutation } from '@tanstack/react-query';
-import { useForm } from 'react-hook-form';
+import { Fragment } from 'react';
 import { DevTool } from '@hookform/devtools';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { z } from 'zod';
 import { GetPropertiesSuccessReply } from 'server/types';
 
-import {
-  Button,
-  Checkbox,
-  FieldError,
-  Fieldset,
-  Form,
-  Input,
-  InputLabel,
-  Radio,
-  Textarea,
-} from '@components';
-import { useDebouncedInput, useLibraryStatus, useServices } from '@hooks';
-import { LibraryInfo } from '@services';
+import { Button, FieldError, Fieldset, Form, Input, InputLabel, Textarea } from '@components';
+
+import { useAddLibraryForm, useLibraryInput } from './hooks';
 
 interface Props {
   properties: GetPropertiesSuccessReply['properties'];
 }
 
-interface ValidationOptions {
-  required: boolean;
-}
-
-const stringValidation = (options: ValidationOptions) => {
-  if (options.required) {
-    return z.string().trim().min(1, { message: 'Required' });
-  }
-
-  return z.string().trim().or(z.null());
-};
-
-const arrayValidation = (options: ValidationOptions) => {
-  if (options.required) {
-    return z.string().trim().array().nonempty({ message: 'Required' });
-  }
-
-  return z.string().trim().array().or(z.null());
-};
-
-const typeConfig = {
-  text: { Element: Input, validation: stringValidation },
-  multiline_text: { Element: Textarea, validation: stringValidation },
-  select: { Element: Radio, validation: stringValidation },
-  multi_select: { Element: Checkbox, validation: arrayValidation },
-};
-
 export const LibraryForm = ({ properties }: Props) => {
-  const { libraryService } = useServices();
-
-  const schema = useMemo(() => {
-    if (!properties) {
-      return z.object({});
-    }
-
-    const validationObject = properties.reduce((acc, curr) => {
-      return {
-        ...acc,
-        [curr.name]: typeConfig[curr.type].validation({ required: curr.required }),
-      };
-    }, {});
-
-    return z.object(validationObject);
-  }, [properties]);
-
   const {
-    register,
-    handleSubmit,
-    control,
-    formState: { errors },
-    reset,
-  } = useForm({
-    mode: 'onSubmit',
-    reValidateMode: 'onChange',
-    resolver: zodResolver(schema),
-  });
-
-  const { setLibrary, existOnDb, existOnNPM } = useLibraryStatus();
-
-  const libraryMutation = useMutation({
-    mutationFn: async (library: LibraryInfo) => {
-      const { data } = await libraryService.add(library).response;
-      return data;
+    getElementByType,
+    formControl: {
+      control,
+      formState: { errors },
+      isSubmitting,
+      register,
+      onSubmit,
     },
+  } = useAddLibraryForm(properties);
+
+  const { error, handleChange } = useLibraryInput({
+    validationErrorMessage: errors.name?.message as string | undefined,
   });
-
-  const { value, setValue } = useDebouncedInput({
-    onSearch: (value) => {
-      setLibrary(value.trim());
-    },
-    wait: 300,
-  });
-
-  const constructInputErrorMessage = (
-    libraryName: string,
-    validationErrorMessage: string | undefined,
-    existOnDb: boolean | null,
-    existOnNPM: boolean | null,
-  ): string => {
-    if (existOnDb !== null && existOnDb) {
-      return `${libraryName} уже есть в базе`;
-    }
-
-    if (validationErrorMessage) {
-      return validationErrorMessage;
-    }
-
-    if (existOnNPM !== null && !existOnNPM) {
-      return `Библиотека ${libraryName} не найдена в NPM`;
-    }
-
-    return '';
-  };
-
-  const handleLibraryNameChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    setValue(event.target.value);
-  };
 
   return (
     <>
-      <Form
-        onSubmit={handleSubmit((data) => {
-          const validEntries = Object.entries(data).filter(([name, value]) => Boolean(value));
-          const dataToSend = Object.fromEntries(validEntries) as LibraryInfo;
-
-          libraryMutation.mutate(dataToSend, {
-            onSuccess: () => {
-              reset();
-              setValue('');
-            },
-          });
-        })}
-      >
+      <Form onSubmit={onSubmit}>
         {properties.map(({ id, name, required, type, ...property }) => {
           const requiredMark = required ? '*' : '';
           const label = `${property.label}${requiredMark}`.trim();
           const errorMessage = errors[name]?.message as string | undefined;
 
           if (type === 'text') {
-            const inputError = constructInputErrorMessage(
-              value.trim(),
-              errorMessage,
-              existOnDb,
-              existOnNPM,
-            );
             return (
               <div key={id} className="form-control w-full">
                 <InputLabel label={label} />
-                <Input {...register(name, { value, onChange: handleLibraryNameChange })} />
-                {inputError && <FieldError message={inputError} />}
+                <Input {...register(name, { onChange: handleChange })} />
+                {error && <FieldError message={error} />}
               </div>
             );
           }
@@ -170,7 +56,7 @@ export const LibraryForm = ({ properties }: Props) => {
           return (
             <Fieldset key={id} label={label} errorMessage={errorMessage}>
               {property.value?.map((value) => {
-                const { Element } = typeConfig[type];
+                const Element = getElementByType(type);
                 return (
                   <Element
                     key={value.id}
@@ -183,7 +69,7 @@ export const LibraryForm = ({ properties }: Props) => {
             </Fieldset>
           );
         })}
-        <Button type="submit" loading={libraryMutation.isLoading}>
+        <Button type="submit" loading={isSubmitting}>
           Добавить
         </Button>
       </Form>
