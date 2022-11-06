7cebf042e7b5fd8843648be0d837e8c682f4ae17
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-06 18:17:28 +0400

    chore(client): improve config

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite';
+import { defineConfig, splitVendorChunkPlugin } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [splitVendorChunkPlugin(), tsconfigPaths(), react()],
+  build: {
+    chunkSizeWarningLimit: 200,
+    target: 'esnext',
+    polyfillModulePreload: false,
+    cssCodeSplit: true,
+  },
 });
