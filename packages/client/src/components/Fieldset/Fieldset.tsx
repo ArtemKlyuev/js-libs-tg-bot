524ad2c78a3374facf0926a5e9348ea7a2577cae
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-06 16:19:26 +0400

    feat(client/components): add `FieldError`

--- packages/client/src/components/Fieldset/Fieldset.tsx
+++ packages/client/src/components/Fieldset/Fieldset.tsx
@@ -1,12 +1,16 @@
+import { FieldError } from '../FieldError';
+
 interface Props {
   label: string;
-  children: React.ReactNode;
+  children: React.ReactNode | React.ReactNode[];
+  errorMessage?: string | undefined;
 }
 
-export const Fieldset = ({ children, label }: Props) => {
+export const Fieldset = ({ children, label, errorMessage }: Props) => {
   return (
     <fieldset>
       <legend>{label}</legend>
+      {errorMessage && <FieldError message={errorMessage} />}
       {children}
     </fieldset>
   );
