a81b2082f33ed5d852db03cd478f9b13c39be5e8
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-07 00:37:53 +0400

    refactor(client/pages): move logic to hooks

---
+++ packages/client/src/pages/AddLibrary/hooks/useAddLibraryForm.ts
@@ -0,0 +1,77 @@
+import { useMemo } from 'react';
+import { useMutation } from '@tanstack/react-query';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { GetPropertiesSuccessReply } from 'server/types';
+
+import { LibraryInfo } from '@services';
+import { useServices } from '@hooks';
+import { Checkbox, Input, Radio, Textarea } from '@components';
+
+interface ValidationOptions {
+  required: boolean;
+}
+
+const stringValidation = (options: ValidationOptions) => {
+  if (options.required) {
+    return z.string().trim().min(1, { message: 'Required' });
+  }
+
+  return z.string().trim().or(z.null());
+};
+
+const arrayValidation = (options: ValidationOptions) => {
+  if (options.required) {
+    return z.string().trim().array().nonempty({ message: 'Required' });
+  }
+
+  return z.string().trim().array().or(z.null());
+};
+
+const typeConfig = {
+  text: { Element: Input, validation: stringValidation },
+  multiline_text: { Element: Textarea, validation: stringValidation },
+  select: { Element: Radio, validation: stringValidation },
+  multi_select: { Element: Checkbox, validation: arrayValidation },
+};
+
+const getElementByType = (type: keyof typeof typeConfig) => typeConfig[type].Element;
+
+export const useAddLibraryForm = (properties: GetPropertiesSuccessReply['properties']) => {
+  const schema = useMemo(() => {
+    if (!properties) {
+      return z.object({});
+    }
+
+    const validationObject = properties.reduce((acc, curr) => {
+      return {
+        ...acc,
+        [curr.name]: typeConfig[curr.type].validation({ required: curr.required }),
+      };
+    }, {});
+
+    return z.object(validationObject);
+  }, [properties]);
+
+  const { handleSubmit, ...formControl } = useForm({
+    mode: 'onSubmit',
+    reValidateMode: 'onChange',
+    resolver: zodResolver(schema),
+  });
+
+  const { libraryService } = useServices();
+
+  const { mutate: send, isLoading: isSubmitting } = useMutation({
+    mutationFn: (library: LibraryInfo) => libraryService.add(library).response,
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    const validEntries = Object.entries(data).filter(([name, value]) => Boolean(value));
+    const dataToSend = Object.fromEntries(validEntries) as LibraryInfo;
+
+    send(dataToSend, { onSuccess: () => formControl.reset() });
+  });
+
+  return { getElementByType, formControl: { ...formControl, onSubmit, isSubmitting } };
+};
---
+++ packages/client/src/pages/AddLibrary/hooks/useLibraryInput.ts
@@ -0,0 +1,53 @@
+import { useDebouncedInput, useLibraryStatus } from '@hooks';
+
+interface Config {
+  validationErrorMessage?: string | undefined;
+}
+
+type HandleChange = (event: React.ChangeEvent<HTMLInputElement>) => void;
+
+interface UseLibraryInputReturn {
+  error: string;
+  handleChange: HandleChange;
+}
+
+const constructInputErrorMessage = (
+  libraryName: string,
+  validationErrorMessage: string | undefined,
+  existOnDb: boolean | null,
+  existOnNPM: boolean | null,
+): string => {
+  if (existOnDb !== null && existOnDb) {
+    return `${libraryName} уже есть в базе`;
+  }
+
+  if (validationErrorMessage) {
+    return validationErrorMessage;
+  }
+
+  if (existOnNPM !== null && !existOnNPM) {
+    return `Библиотека ${libraryName} не найдена в NPM`;
+  }
+
+  return '';
+};
+
+export const useLibraryInput = ({ validationErrorMessage }: Config): UseLibraryInputReturn => {
+  const { setLibrary, existOnDb, existOnNPM } = useLibraryStatus();
+
+  const { value, setValue } = useDebouncedInput({
+    onSearch: (value) => setLibrary(value.trim()),
+    wait: 300,
+  });
+
+  const handleChange: HandleChange = (event) => setValue(event.target.value);
+
+  const error = constructInputErrorMessage(
+    value.trim(),
+    validationErrorMessage,
+    existOnDb,
+    existOnNPM,
+  );
+
+  return { error, handleChange };
+};
