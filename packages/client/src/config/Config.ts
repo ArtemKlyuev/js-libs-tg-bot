dc35552c0acfe145bf7792721320b0f474d55847
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-11 19:54:37 +0400

    feat(client): create `Config`

---
+++ packages/client/src/config/Config.ts
@@ -0,0 +1,31 @@
+interface Env {
+  BACKEND_URL: string;
+  MODE: 'development' | 'production';
+  DEV: boolean;
+  PROD: boolean;
+  BASE_URL: string;
+}
+
+interface Config {
+  env: Env;
+}
+
+const ENV_PREFIX = 'APP_EXPOSE_';
+
+class AppConfig implements Config {
+  #env: Env;
+
+  constructor() {
+    this.#env = Object.entries(import.meta.env).reduce<Env>((acc, [metaKey, value]) => {
+      const key = metaKey.replace(ENV_PREFIX, '');
+
+      return { ...acc, [key]: value };
+    }, {} as Env);
+  }
+
+  get env(): Env {
+    return this.#env;
+  }
+}
+
+export const config = new AppConfig();
