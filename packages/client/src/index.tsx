578e15cedec77260fe168a2d423a1d4d4d17ff9d
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-11 21:11:12 +0400

    refactor(client): move to `App`

--- packages/client/src/index.tsx
+++ packages/client/src/index.tsx
@@ -1,86 +1,14 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React, { lazy } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { AxiosHttpRequest } from 'common/services';
-
-import { Error } from '@pages';
-import { Layout } from '@components';
-import { ServicesProvider } from '@hooks';
-import { LibraryService } from '@services';
-
-import { config } from './config';
 
 import { App } from './App';
 
 import './index.scss';
 
-const SearchLibraryPage = lazy(() =>
-  import('./pages/SearchLibrary').then((module) => ({
-    default: module.SearchLibrary,
-  })),
-);
-
-const AddLibraryPage = lazy(() =>
-  import('./pages/AddLibrary').then((module) => ({ default: module.AddLibrary })),
-);
-
 const root = document.getElementById('root')!;
-const queryClient = new QueryClient({
-  defaultOptions: { queries: { retry: false, refetchOnWindowFocus: false } },
-});
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout title="Root" />,
-    errorElement: <Error />,
-  },
-  {
-    path: '/library',
-    children: [
-      {
-        path: 'add',
-        element: (
-          <Layout title="Добавление библиотеки">
-            <AddLibraryPage />
-          </Layout>
-        ),
-      },
-      {
-        path: 'search',
-        element: (
-          <Layout title="Поиск библиотеки">
-            <SearchLibraryPage />
-          </Layout>
-        ),
-      },
-    ],
-  },
-]);
-
-const httpRequest = new AxiosHttpRequest({
-  baseURL: `${config.env.BACKEND_URL}/api`,
-  withCredentials: true,
-});
-
-const libraryService = new LibraryService(httpRequest);
-const telegram = window.Telegram.WebApp;
-
-const services = {
-  config,
-  httpRequest,
-  libraryService,
-  telegram,
-};
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      {/* <App /> */}
-      <ServicesProvider value={services}>
-        <RouterProvider router={router} />
-      </ServicesProvider>
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>,
 );
