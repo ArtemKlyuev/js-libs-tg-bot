42280834d90e3c01e138de884430a68f0f3976cd
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-12 03:36:07 +0400

    feat(server/controllers): sort props

--- packages/server/src/controllers/searchLibraryByQuery/searchLibraryByQuery.ts
+++ packages/server/src/controllers/searchLibraryByQuery/searchLibraryByQuery.ts
@@ -1,5 +1,23 @@
-// import { SearchResult } from '@repository';
 import { DatabaseRepository, SearchResult } from '@repository';
+
+interface NameToConfig {
+  [name: string]: {
+    order: number;
+  };
+}
+
+const nameToConfig: NameToConfig = {
+  name: { order: 1 },
+  platform: { order: 2 },
+  summary: { order: 3 },
+  'repo link': { order: 4 },
+  tags: { order: 5 },
+  'npm weekly downloads': { order: 6 },
+  'github stars': { order: 7 },
+  status: { order: 8 },
+  'score /5': { order: 9 },
+  review: { order: 10 },
+};
 
 export const searchLibraryByQuery = async (
   // TODO: add filters
@@ -8,5 +26,14 @@ export const searchLibraryByQuery = async (
 ): Promise<SearchResult> => {
   const eitherLibrary = await databaseRepository.searchLibraries(query);
 
-  return eitherLibrary;
+  return eitherLibrary.mapRight((libs) =>
+    libs.map((props) =>
+      props.sort((propA, propB) => {
+        const orderA = nameToConfig[propA.name.toLowerCase()].order;
+        const orderB = nameToConfig[propB.name.toLowerCase()].order;
+
+        return orderA - orderB;
+      }),
+    ),
+  );
 };
