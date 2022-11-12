39a1d128f697d51b77bd2500c460ba66e55b6eda
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-12 23:21:45 +0400

    feat(client/components): add `selected` prop

--- packages/client/src/components/LibraryCard/LibraryCard.tsx
+++ packages/client/src/components/LibraryCard/LibraryCard.tsx
@@ -1,13 +1,22 @@
+import cn from 'classnames';
+
 interface Props {
   properties: { id: string; name: string; value: string }[];
+  selected?: boolean;
   onClick?: () => void;
 }
 
 const isLink = (value: string): boolean => value.startsWith('https') || value.startsWith('http');
 
-export const LibraryCard = ({ properties, onClick }: Props) => {
+export const LibraryCard = ({ properties, selected, onClick }: Props) => {
+  const classNames = cn(
+    'card',
+    'bg-neutral',
+    selected ? 'bg-primary text-primary-content' : 'bg-neutral text-neutral-content',
+  );
+
   return (
-    <button type="button" onClick={onClick} className="card bg-neutral text-neutral-content">
+    <button type="button" onClick={onClick} className={classNames}>
       <div className="card-body gap-[15px]">
         {properties.map(({ id, name, value }) => (
           <div key={id} className="text-left">
