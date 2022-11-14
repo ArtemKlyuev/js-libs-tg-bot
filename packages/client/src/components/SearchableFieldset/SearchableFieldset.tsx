f8b37439a1e646aff8bbfded1dc2add773bcbae8
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2022-11-15 00:23:50 +0400

    refactor(client/components): reduce logic complexity

--- packages/client/src/components/SearchableFieldset/SearchableFieldset.tsx
+++ packages/client/src/components/SearchableFieldset/SearchableFieldset.tsx
@@ -9,65 +9,26 @@ interface Props extends Omit<FieldsetProps, 'children'> {
   children: (data: any[]) => React.ReactNode;
 }
 
-class Data {
-  #data: any;
-  #matchesSearch: boolean = true;
-
-  constructor(data: any) {
-    this.#data = data;
-  }
-
-  setMatchesSearch(): void {
-    this.#matchesSearch = true;
-  }
-
-  unMatchesSearch(): void {
-    this.#matchesSearch = false;
-  }
-
-  get data(): any {
-    return this.#data;
-  }
-
-  get matchesSearch(): boolean {
-    return this.#matchesSearch;
-  }
-}
-
-const mapData = (data: any[]): Data[] => data.map((item) => new Data(item));
-
 export const SearchableFieldset = ({ data, children, searchBy, ...props }: Props) => {
-  const [search, setSearch] = useState(mapData(data));
-  const fuse = useMemo(
-    () =>
-      new Fuse(
-        search.map(({ data }) => data),
-        { keys: [searchBy] },
-      ),
-    [],
-  );
+  const [searchableData, setSearchableData] = useState(data);
+  const fuse = useMemo(() => new Fuse(data, { keys: [searchBy] }), []);
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    const finded = fuse.search(e.target.value);
-    const hasItemUnmatchesSearch = search.some((data) => !data.matchesSearch);
-    const copy = hasItemUnmatchesSearch ? [...search] : search;
+    const searchResult = fuse.search(e.target.value);
 
-    if (!finded.length) {
-      copy.forEach((data) => data.setMatchesSearch());
-    } else {
-      copy.forEach((data) => data.unMatchesSearch());
-      finded.forEach(({ refIndex }) => copy[refIndex].setMatchesSearch());
+    if (searchResult.length) {
+      const newState = searchResult.map(({ item }) => item);
+      setSearchableData(newState);
+      return;
     }
 
-    setSearch(copy);
+    setSearchableData(data);
   };
 
   return (
     <Fieldset {...props}>
       <Input type="search" placeholder="Search" onChange={handleSearch} />
-      <div className="max-h-[300px] overflow-y-auto">
-        {children(search.filter(({ matchesSearch }) => matchesSearch).map(({ data }) => data))}
-      </div>
+      <div className="max-h-[300px] overflow-y-auto">{children(searchableData)}</div>
     </Fieldset>
   );
 };
