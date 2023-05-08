4f90655a739cad92997b78ea9a6e4559f85f12ee
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-05-08 13:56:11 +0400

    chore: update imports

--- packages/client/src/pages/AddLibrary/hooks/useAddLibraryForm.ts
+++ packages/client/src/pages/AddLibrary/hooks/useAddLibraryForm.ts
@@ -3,6 +3,7 @@ import { useMutation } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
+import { HttpRequestError } from 'common/services';
 import {
   GetPropertiesSuccessReply,
   AddLibrarySuccessResponse,
@@ -12,7 +13,6 @@ import {
 import { LibraryInfo } from '@services';
 import { useServices } from '@hooks';
 import { Checkbox, Input, Radio, Textarea } from '@components';
-import { HttpRequestError } from 'common/services';
 
 interface ValidationOptions {
   required: boolean;
