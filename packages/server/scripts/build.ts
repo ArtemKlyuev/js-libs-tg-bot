822d154a1f06afcdd2c89f17504ad26bf661c892
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-05-08 13:54:11 +0400

    chore: server build

--- packages/server/scripts/build.ts
+++ packages/server/scripts/build.ts
@@ -1,17 +1,54 @@
 import { build } from 'esbuild';
 
-import { devDependencies } from '../package.json';
+import packageJSON from '../package.json' assert { type: 'json' };
+
+const devDeps = Object.keys(packageJSON.devDependencies);
+const deps = Object.keys(packageJSON.dependencies).filter((dep) => dep !== 'common');
 
 build({
   entryPoints: ['./src/index.ts'],
-  outdir: 'build',
-  external: Object.keys(devDependencies),
+  outfile: 'build/index.js',
+  external: [...devDeps, ...deps],
   platform: 'node',
-  format: 'cjs',
-  target: 'es2022',
+  format: 'esm',
+  target: 'node18.16.0',
   bundle: true,
   minify: false,
   sourcemap: false,
+  banner: {
+    js: [
+      `import { createRequire as topLevelCreateRequire } from 'module';`,
+      `import { fileURLToPath } from 'url';`,
+      `import { dirname } from 'path';`,
+      `const __filename = fileURLToPath(import.meta.url);`,
+      `const __dirname = dirname(__filename);`,
+      `const require = topLevelCreateRequire(import.meta.url);`,
+    ].join('\n'),
+  },
 })
   .then(() => console.log('Сервер успешно собран!'))
   .catch((error) => console.error(`Ошибка при сборке сервера! ${error}`));
+
+// build({
+//   entryPoints: ['./src/index.ts'],
+//   outfile: 'build/index.cjs',
+//   external: Object.keys(packageJSON.devDependencies),
+//   platform: 'node',
+//   format: 'cjs',
+//   target: 'node16.17.1',
+//   bundle: true,
+//   minify: false,
+//   sourcemap: false,
+//   // banner: {
+//   //   js: [
+//   //     `import { createRequire as topLevelCreateRequire } from 'module'`,
+//   //     `import { fileURLToPath } from 'url';`,
+//   //     `import { dirname } from 'path';`,
+//   //     `const __filename = fileURLToPath(import.meta.url);`,
+//   //     `const __dirname = dirname(__filename);`,
+//   //     `const require = topLevelCreateRequire(import.meta.url)`,
+//   //   ].join('\n'),
+//   // },
+// })
+//   .then(() => console.log('Сервер успешно собран!'))
+//   .catch((error) => console.error(`Ошибка при сборке сервера! ${error}`));
