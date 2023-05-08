f0c8de7d8e436030ad609562555473ba146492de
ArtemKlyuev <36650201+ArtemKlyuev@users.noreply.github.com>
2023-05-08 13:54:48 +0400

    chore: wrap main

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -9,41 +9,45 @@ import { createApp } from './app';
 import { apiRoutes } from './routes';
 import { createDIContainer } from './diContainer';
 
-const app = createApp();
+const configApp = async () => {
+  const app = createApp();
 
-app.register(cors, {
-  origin: Config.isDev ? '*' : Config.env.ALLOWED_ORIGINS,
-  credentials: true,
-});
+  app.register(cors, {
+    origin: Config.isDev ? '*' : Config.env.ALLOWED_ORIGINS,
+    credentials: true,
+  });
 
-await app.register(auth);
+  await app.register(auth);
 
-const validate: FastifyBasicAuthOptions['validate'] = async (username, password) => {
-  if (Number(username) === Config.env.LOGIN && password === Config.env.PASSWORD) {
-    return;
-  }
+  const validate: FastifyBasicAuthOptions['validate'] = async (username, password) => {
+    if (Number(username) === Config.env.LOGIN && password === Config.env.PASSWORD) {
+      return;
+    }
 
-  return new Error('Invalid credentials');
+    return new Error('Invalid credentials');
+  };
+
+  await app.register(basicAuth, { validate });
+
+  app.addHook('onRequest', app.auth([app.basicAuth]));
+
+  app.register(fastifyAwilixPlugin, { disposeOnClose: true, disposeOnResponse: false });
+  createDIContainer();
+  app.register(apiRoutes, { prefix: '/api' });
+
+  app.get('/healthcheck', async () => {
+    return 'ok';
+  });
+
+  app.setErrorHandler(function (err, req, reply) {
+    if (err.statusCode === 401) {
+      return reply.code(401).send('unauthorized');
+    }
+
+    return reply.send(`Unknown error!\n${err}`);
+  });
+
+  app.start();
 };
 
-await app.register(basicAuth, { validate });
-
-app.addHook('onRequest', app.auth([app.basicAuth]));
-
-app.register(fastifyAwilixPlugin, { disposeOnClose: true, disposeOnResponse: false });
-createDIContainer();
-app.register(apiRoutes, { prefix: '/api' });
-
-app.get('/healthcheck', async () => {
-  return 'ok';
-});
-
-app.setErrorHandler(function (err, req, reply) {
-  if (err.statusCode === 401) {
-    return reply.code(401).send('unauthorized');
-  }
-
-  return reply.send(`Unknown error!\n${err}`);
-});
-
-app.start();
+configApp();
